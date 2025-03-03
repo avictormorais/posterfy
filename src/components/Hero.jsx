47147967b61e917da898e01bb75c541418997206
unknown <victormm.dev@gmail.com>
2025-03-03 11:08:47 -0300

    styles: create new hero design

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,55 +2,72 @@ import styled from 'styled-components';
 import Icon from './icons/icon';
 import Albuns from '../assets/albuns.png';
 import { useTranslation } from 'react-i18next';
+import { MdOutlineKeyboardDoubleArrowDown } from "react-icons/md";
 
 const HeroDiv = styled.div`
-    margin-top: 200px;
-    width: 80%;
+    width: 100%;
     display: flex;
+    flex-direction: column;
+    align-items: center;
     overflow: hidden;
     margin-inline: auto;
+    position: relative;
+    text-align: center;
+    height: 100vh;
+    align-items: center;
 `;
 
 const Container = styled.div`
     width: 100%;
     max-width: 100%;
     display: flex;
-    justify-content: center;
     flex-direction: column;
     align-items: center;
-    padding-left: 30px;
+    padding: 0 30px;
+    margin-block: auto;
 
     @media (max-width: 550px) {
-        padding-left: 0px;
+        padding: 0;
     }
 `;
 
 const ContainerIcon = styled.div`
     max-width: 100%;
-    margin-inline: auto;
-    margin-right: 50px;
+    margin: 50px 0;
     display: flex;
     justify-content: center;
-    flex-direction: column;
-    align-items: end;
-    padding-left: auto;
     animation: heartbeat 1.85s infinite;
+    margin-top: 100px;
+
+    &:before {
+        content: '';
+        position: absolute;
+        width: 280px;
+        height: 280px;
+        border-radius: 50%;
+        background: #01b755;
+        filter: blur(100px);
+        z-index: -10;
+        margin-top: -50px;
+    }
 
     @keyframes heartbeat {
         0%, 50%, 90%, 100% { opacity: 0.1; transform: scale(1); }
         30%, 70% { opacity: 0.25; transform: scale(1.05); }
     }
-
-
-    @media (max-width: 900px) {
-        display: none;
-    }
 `;
 
 const Title = styled.h1`
-    font-size: 3.5em;
-    margin-right: auto;
-    margin-bottom: 5px;
+    font-size: 5em;
+    margin-bottom: 20px;
+
+    @media (max-width: 900px) {
+        font-size: 4.5em;
+    }
+
+    @media (max-width: 600px) {
+        font-size: 4em;
+    }
 `;
 
 const Paragraph = styled.p`
@@ -58,9 +75,7 @@ const Paragraph = styled.p`
     opacity: 0.5;
     width: 80%;
     font-weight: 500;
-    margin-right: auto;
-    margin-block: 2px;
-
+    margin: 2px 0;
 
     @media (max-width: 900px) {
         width: 90%;
@@ -72,18 +87,32 @@ const DivAlbum = styled.div`
     width: 80%;
     justify-content: center;
     align-items: center;
-    margin-inline: auto;
+    margin: 0px auto;
 `;
 
 const AlbumExample = styled.img`
-    display: flex;
-    margin-inline: auto;
-    margin-block: 100px;
     width: 100%;
+`;
+
+const HeroIcon = styled(MdOutlineKeyboardDoubleArrowDown)`
+    font-size: 2em;
+    color: white;
+    margin: 10px;
+    opacity: 0.2;
+    position: absolute;
+    bottom: 7%;
 `;
 
 function Hero() {
     const { t } = useTranslation();
+
+    const handleScroll = () => {
+        window.scrollTo({
+            top: window.innerHeight - 100,
+            behavior: 'smooth'
+        });
+    }
+
     return (
         <>
             <HeroDiv>
@@ -95,10 +124,13 @@ function Hero() {
                     <Paragraph>
                         {t('paragraphHero2')}
                     </Paragraph>
+
+                    <ContainerIcon>
+                        <Icon fill={'white'} width={'180px'} height={'198.23px'} />
+                    </ContainerIcon>
+
+                    <HeroIcon onClick={handleScroll}/>
                 </Container>
-                <ContainerIcon>
-                    <Icon fill={'white'} width={'180px'} height={'198.23px'} />
-                </ContainerIcon>
             </HeroDiv>
             <DivAlbum>
                 <AlbumExample src={Albuns} />
