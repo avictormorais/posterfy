4258abeb4536b14817350e7318505db0f71898a8
avictormorais <victormm.dev@gmail.com>
2025-04-18 09:14:22 -0300

    feat: new posters on hero, featuring custom font

--- src/components/albumData.js
+++ src/components/albumData.js
@@ -1,48 +1,28 @@
-import divideImg from "../assets/albunsHero/Posterfy - ÷ (Deluxe).png"
-import hardstoneImg from "../assets/albunsHero/Posterfy - HARDSTONE PSYCHO.png"
-import jesusIsKingImg from "../assets/albunsHero/Posterfy - JESUS IS KING.png"
-import starboyImg from "../assets/albunsHero/Posterfy - Starboy.png"
-import heartbreakImg from "../assets/albunsHero/Posterfy - 808s & Heartbreak.png"
+import IMG1 from "../assets/albunsHero/Posterfy - KTT ZOO.png"
+import IMG2 from "../assets/albunsHero/Posterfy - STARBOY.png"
+import IMG3 from "../assets/albunsHero/Posterfy - CHROMAKOPIA.png"
+import IMG4 from "../assets/albunsHero/Posterfy - ZIMA.png"
+import IMG5 from "../assets/albunsHero/Posterfy - Sobrevivendo no inferno.png"
 
 export const albumData = [
   {
     id: 1,
-    artist: "Ed Sheeran",
-    title: "Divide",
-    coverImage: divideImg,
-    releaseDate: "2017-03-03",
-    runtime: "59min 33s",
+    coverImage: IMG1,
   },
   {
     id: 2,
-    artist: "Don Toliver",
-    title: "Hardstone P",
-    coverImage: hardstoneImg,
-    releaseDate: "2023-05-16",
-    runtime: "51min 02s",
+    coverImage: IMG2,
   },
   {
     id: 3,
-    artist: "Kanye West",
-    title: "Jesus Is King",
-    coverImage: jesusIsKingImg,
-    releaseDate: "2019-10-25",
-    runtime: "27min 4s",
+    coverImage: IMG3,
   },
   {
     id: 4,
-    artist: "The Weeknd",
-    title: "Starboy",
-    coverImage: starboyImg,
-    releaseDate: "2016-11-25",
-    runtime: "68min 36s",
+    coverImage: IMG4,
   },
   {
     id: 5,
-    artist: "Kanye West",
-    title: "808s & Heartbreak",
-    coverImage: heartbreakImg,
-    releaseDate: "2008-11-24",
-    runtime: "52min 01s",
+    coverImage: IMG5,
   },
 ]
