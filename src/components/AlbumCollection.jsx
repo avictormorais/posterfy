bd8966399c48bb23c467ae225c2c358e70ccd477
Benno <107504783+BennoCrafter@users.noreply.github.com>
2025-08-19 18:01:08 +0200

    fix: fix import paths in AlbumCollection and CanvasPoster components (#6)

--- src/components/AlbumCollection.jsx
+++ src/components/AlbumCollection.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 import styled from "styled-components"
-import { albumData } from "./AlbumData"
+import { albumData } from "./albumData"
 import { useTranslation } from "react-i18next"
 
 const AlbumsContainer = styled.div`
