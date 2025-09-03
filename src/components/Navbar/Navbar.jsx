349b2b267c238c52ee0ada85c780952afc4bfde8
avictormorais <victormm.dev@gmail.com>
2025-09-03 14:37:20 -0300

    styles: smooth navbar style

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,10 +11,9 @@ const NavbarContainer = styled.header`
   width: 100%;
   z-index: 50;
   transition: all 0.3s ease;
-  background-color: ${({ scrolled }) => (scrolled ? "rgba(0, 0, 0, 0.15)" : "transparent")};
   backdrop-filter: ${({ scrolled }) => (scrolled ? "blur(20px)" : "none")};
   padding: ${({ scrolled }) => (scrolled ? "10px 0" : "20px 0")};
-  box-shadow: ${({ scrolled }) => (scrolled ? "0 4px 30px var(--shadowColor)" : "none")};
+  box-shadow: ${({ scrolled }) => (scrolled ? "0 2px 15px var(--shadowColor)" : "none")};
   transform: ${({ visible }) => (visible ? "translateY(0)" : "translateY(-100%)")};
 `
 
